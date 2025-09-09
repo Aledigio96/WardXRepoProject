0189a9fb79bc4e724a70c36d4ae32801c74d4ee8
Aledigio96 <alessandro.digiovanni10@gmail.com>
2025-09-09 09:54:55 +0200

    Add cors configurations

--- BackEnd/demo/src/main/java/alessandro/digiovanni/demo/configurations/SecurityConfig.java
+++ BackEnd/demo/src/main/java/alessandro/digiovanni/demo/configurations/SecurityConfig.java
@@ -1,5 +1,6 @@
 package alessandro.digiovanni.demo.configurations;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,6 +11,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +36,7 @@ public class SecurityConfig {
 
         httpSecurity.cors(Customizer.withDefaults());
 
+
         return httpSecurity.build();
 
     }
@@ -36,4 +44,19 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder getBCrypt() {
         return new BCryptPasswordEncoder(12);
-    }}
+    }
+
+
+@Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+    CorsConfiguration configuration= new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList ("http://localhost:5173", "http://localhost:5173"));
+    configuration.setAllowedMethods(List.of("*"));
+    configuration.setAllowedHeaders(List.of("*"));
+    UrlBasedCorsConfigurationSource source= new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**",configuration);
+    return source;
+}
+
+}
+
